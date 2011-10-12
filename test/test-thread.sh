8540aff985b96f7f77c1cdc6a54276e1b0a85d43
Patrice FERLET <metal3d@gmail.com>
2011-10-12 04:22:43 +0200

    Add one more thread Change random sleep time by getting a random value lower than 8 seconds

--- test/test-thread.sh
+++ test/test-thread.sh
@@ -12,24 +12,32 @@ MyThread=(
 )
 
 MyThread::run(){
-    echo "Run !"
-    local sleep_time=$((RANDOM/2500))
-    echo "I will work for $sleep_time seconds..."
-    sleep $sleep_time
+    local  _r=$RANDOM
+    #find a nice random value... 
+    while [ $_r -gt 8 ]; do ((_r=_r-(_r/2))); done
+    [[ $_r == 0 ]] && _r=1
+    
+    #information
+    echo "I will work for $_r seconds..."
+    sleep $_r
     echo "Stopped !"
 }
 
-#create 2 threads
+#create 3 threads
 new MyThread T1
 new MyThread T2
+new MyThread T3
 
 #Start threads
 T1.start
 T2.start
+T3.start
 
 #wait each threads...
 T1.join
 T2.join
+T3.join
+
 
 #and there we are...
 echo "done"

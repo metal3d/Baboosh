cb6425a2e5930561918c8a32bc296f33ee58c24d
Patrice FERLET <metal3d@gmail.com>
2011-10-12 04:43:22 +0200

    Add new tests for __delete__ and __kill__ methods

--- test/test-human.sh
+++ test/test-human.sh
@@ -7,6 +7,7 @@ source $(dirname $0)/../src//baboosh.sh
 Human=(
     function eat
     function sleep
+    function die
     var name
     var age
 )
@@ -16,6 +17,13 @@ Human::__init__(){
 
     eval $this.set_name "$1"
     eval $this.set_age "$2"
+}
+
+Human::die(){
+    local this=$1; shift
+    local name=$(eval $this.name)
+
+    echo "hhaaarrrgg $name is dead"
 }
 
 Human::eat(){
@@ -28,6 +36,16 @@ Human::sleep(){
     local this=$1; shift
     local name=$(eval $this.name)
     echo $name is sleeping now...
+}
+
+Human::__kill__(){
+    #if script is killed, human must die !
+    local this=$1; shift    
+    eval $this.die
+}
+
+Human::__delete__(){
+    echo "When script exits, you can do whatever you want in this method"
 }
 
 #trust me, he is human
@@ -45,3 +63,9 @@ jcvd.set_age 46
 
 #print age
 echo  Now: $(jcvd.name) is $(jcvd.age) years old
+
+echo "You have 5 seconds to kill humans by pressing CTRL+C..."
+sleep 5
+
+echo "Well ! nobody is dead"
+exit 0
